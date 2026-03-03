a61e3c672ea3cf3c559440701a44e8692cfbafc9
Mallikarjun Shankar <shankarmallikarjun@hotmail.com>
2026-03-03 10:06:43 +0530

    Level One: Problem Five and Six

---
+++ WeekOne/LevelOne/Q5.java
@@ -0,0 +1,6 @@
+public class Q5 {
+    public static void main (String[] args){
+        int NoofPens = 14, NoofStudents = 3;
+        System.out.println("The Pen Per Student is " + (NoofPens / NoofStudents) + " and the remaining pen not distributed is " + (NoofPens % NoofStudents));
+    }
+}
