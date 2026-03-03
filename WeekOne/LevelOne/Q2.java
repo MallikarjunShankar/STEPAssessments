397e7aa91e6b1a4c6b5bbf51e15a5a746afcb683
Mallikarjun Shankar <shankarmallikarjun@hotmail.com>
2026-03-03 10:01:15 +0530

    Level One: Problem Two

---
+++ WeekOne/LevelOne/Q2.java
@@ -0,0 +1,8 @@
+public class Q2 {
+    public static void main(String[] args) {
+        int MathsMarks = 94, PhysicsMarks = 95, ChemistryMarks = 96;
+        int TotalMarks = MathsMarks + PhysicsMarks + ChemistryMarks;
+        int AverageMarks = TotalMarks / 3;
+        System.out.println("Sam's average mark in PCM is " + AverageMarks);
+    }
+}
